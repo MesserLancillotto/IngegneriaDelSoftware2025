46d2ee3864350a052e6c5d1453821634803d74a8
Lancillotto dal Lago <a.lemme99@gmail.com>
2025-10-23 10:18:17 +0200

    Pronto per merge sul main

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -9,14 +9,14 @@ public class Server
 {
     public static void main(String [] args) 
     {
-        while(true) 
+        while(true)
         {
             ServerAPI api = new ServerAPI();
             api.start(); 
             try {
                 api.join();
             } catch (Exception e) {
-            System.out.println("An error occurred: " + e);
+                System.out.println("An error occurred: " + e);
             }
         }
     }
